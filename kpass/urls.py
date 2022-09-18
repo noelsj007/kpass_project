1e9340f9ee77c3fd81f5ba75ceaa008d4137935e
noelsj <noel.s.jacob2002@gmail.com>
2022-09-18 15:04:05 +0530

    Admin homepage UI

--- kpass/urls.py
+++ kpass/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path('users/', include('users.urls'))
+    # path("admin/", admin.site.urls),
+    path('', include('users.urls')),
+    path('admin/', include('adminapp.urls'))
 ]
