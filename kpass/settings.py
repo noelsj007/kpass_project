fb2773e349e6ecab8fafd977470d63cf5abf192b
noelsj <noel.s.jacob2002@gmail.com>
2022-10-07 07:32:35 +0530

    edit

--- kpass/settings.py
+++ kpass/settings.py
@@ -114,7 +114,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "users/static/"
+STATIC_URL = "./users/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
