ffc92ad58628b97bdcc7713f2f89d4d944996a74
noelsj <noel.s.jacob2002@gmail.com>
2022-10-08 15:55:25 +0530

    train db

--- irctc/models.py
+++ irctc/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class TrainRoutes(models.Model):
+
+    states = models.CharField(max_length=100, blank=True, null=True)
+    districts = models.CharField(max_length=255, blank= True, null=True)
+    from_place = models.CharField(max_length=100, blank= True, null=True)
+    to_place = models.CharField(max_length=100, blank= True, null=True)
+
+    
+    def __str__(self):
+        return self.states
