425af675cfb2555568d047d8a22917a0507a5688
noelsj <noel.s.jacob2002@gmail.com>
2022-10-08 10:55:13 +0530

    edit

--- kpass/settings.py
+++ kpass/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-)49swnuq!4a4fncyhbf_yj&k-=bec!mwgez%*(nmyx@86*m2ef"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['ekpass.herokuapp.com', 'localhost', '127.0.0.1']
 
@@ -114,7 +114,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "./users/static/"
+STATIC_URL = "/users/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
