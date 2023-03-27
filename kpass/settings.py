033ef6ccd8966a1458f962deec57c7d108478964
noelsj <noel.s.jacob2002@gmail.com>
2023-03-27 12:57:42 +0530

    smtp credential secure

--- kpass/settings.py
+++ kpass/settings.py
@@ -143,5 +143,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'noel.s.jacob2002@gmail.com'
-EMAIL_HOST_PASSWORD = 'tvoviyybjfcobzvb'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
